957697d18577e3e634acdf37e896d76e91f1cedb
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-04-29 08:42:39 -0600

    feat: :sparkles: Creación de contenido en index.html y objetos en main.js
    Creación de contenido en index.html y objetos en main.js que contienen la información de cada habitación

---
+++ main.js
@@ -0,0 +1,38 @@
+const habitacionUno = {
+    numero: 1,
+    tipo: "Sencilla",
+    precioNoche: 100,
+    estado: "libre",
+    huesped: "vacío",
+}
+
+const habitacionDos = {
+    numero: 2,
+    tipo: "Sencilla",
+    precioNoche: 100,
+    estado: "Ocupado",
+    huesped: "Jorge López",
+}
+
+const habitacionTres = {
+    numero: 3,
+    tipo: "Doble",
+    precioNoche: 150,
+    estado: "libre",
+    huesped: "vacío",
+}
+const habitacionCuatro = {
+    numero: 4,
+    tipo: "Doble",
+    precioNoche: 150,
+    estado: "Ocupado",
+    huesped: "Erika Pérez",
+}
+
+const habitacionCinco = {
+    numero: 5,
+    tipo: "Suite",
+    precioNoche: 200,
+    estado: "limpieza",
+    huesped: "vacío",
+}
